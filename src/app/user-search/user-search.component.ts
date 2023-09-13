f09847a916d73b5aa62c1cad43ff3d5f2b13cee0
Johsua Santoy <johsua.santoy@connectit.com.mx>
2023-09-13 05:18:40 -0600

    Se agregaron las validaciones

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { UserDetailsComponent } from '../user-details/user-details.component';
+import { FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-user-search',
@@ -19,14 +21,32 @@ export class UserSearchComponent {
 
   constructor(private http: HttpClient, private modalService: BsModalService) { }
 
-   searchUser() {
-    if (this.searchText) {
-      this.http.get(`https://api.github.com/search/users?q=${this.searchText}&page=${this.currentPage}&per_page=${this.itemsPerPage}`)
-        .subscribe((data) => {
-          this.userData = data;
-          this.totalUsers = this.userData.total_count;
-        });
+  minLengthValidator(value: string): boolean {
+    return !!(value && value.length >= 4);
+  }
+
+  wordValidator(value: string): boolean {
+    return !!(value && !value.toLowerCase().includes('raspberry'));
+  }
+
+  searchUser() {
+    if (!this.searchText) {
+      throw new Error('El nombre de usuario es requerido.');
     }
+    if (!this.minLengthValidator(this.searchText)) {
+      throw new Error('El nombre de usuario debe tener al menos 4 caracteres.');
+    }
+
+    if (!this.wordValidator(this.searchText)) {
+      throw new Error('La palabra "raspberry" está prohibida en el nombre de usuario.');
+    }
+
+    this.http.get(`https://api.github.com/search/users?q=${this.searchText}&page=${this.currentPage}&per_page=${this.itemsPerPage}`)
+      .subscribe((data) => {
+        this.userData = data;
+        this.totalUsers = this.userData.total_count;
+      });
+
   }
 
   previousPage() {
