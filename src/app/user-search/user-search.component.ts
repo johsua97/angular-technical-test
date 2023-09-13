b56892660c7a9ebf8bb749924ee469892d6aa8ab
Johsua Santoy <johsua.santoy@connectit.com.mx>
2023-09-13 02:09:55 -0600

    Se agrego la paginacion para mostrar los primeros 10 usuarios de la busqueda y se agrego en el template html, el user login y user id de cada usuario

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -9,16 +9,35 @@ import { HttpClient } from '@angular/common/http';
 export class UserSearchComponent {
   searchText: string = '';
   userData: any;
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+  totalUsers: number = 0;
+  loading: boolean = false;
+
 
   constructor(private http: HttpClient) { }
 
    searchUser() {
     if (this.searchText) {
-      this.http.get(`https://api.github.com/search/users?q=${this.searchText}`)
+      this.http.get(`https://api.github.com/search/users?q=${this.searchText}&page=${this.currentPage}&per_page=${this.itemsPerPage}`)
         .subscribe((data) => {
-          this.userData =  data;
-          console.log('userData:', this.userData);
+          this.userData = data;
+          this.totalUsers = this.userData.total_count;
         });
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.searchUser();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage * this.itemsPerPage < this.totalUsers) {
+      this.currentPage++;
+      this.searchUser();
     }
   }
 
